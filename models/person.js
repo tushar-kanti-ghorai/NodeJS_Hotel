5d42a1ace818226101e2808f5e3e20574ecced04
tushar-kanti-ghorai <tusharkantighorai643@gmail.com>
2025-03-04 09:38:22 +0530

    Done

--- models/person.js
+++ models/person.js
@@ -29,7 +29,16 @@ const personSchema=new mongoose.Schema({
     salary:{
         type:Number,
         required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
     }
+  
 
 })
 
--- server.js
+++ server.js
@@ -1,27 +1,36 @@
-const express=require('express');
-const app=express();
-const db=require('./db');
+const express = require('express')
+const app = express();
+const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
+
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
 
-const bodyPerser=require('body-parser');
-//Import the router file
-const personRoutes=require('./routes/personRoutes');
-const menuItemRoutes=require('./routes/menuItemRoutes');
-app.use(bodyPerser.json());//it stores the data in req.body
-const PORT=process.env.PORT || 3000
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
+}
+app.use(logRequest);
 
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false})
 
+app.get('/', localAuthMiddleware, function (req, res) {
+    res.send('Welcome to our Hotel');
+})
 
+// Import the router files
+const personRoutes = require('./routes/personRoutes');
+const menuItemRoutes = require('./routes/menuItemRoutes');
 
-
-
-
-//use the routers
-app.use('/person',personRoutes);
-app.use('/menu',menuItemRoutes);
-app.listen(PORT,()=>{
-    console.log("connected to the Port no."+PORT)
-});
-
-
+// Use the routers
+app.use('/person', personRoutes);
+app.use('/menu', menuItemRoutes);
+  
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
