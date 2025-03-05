f3682ae42d20122dba5dea00c6530fa02b9acda7
tushar-kanti-ghorai <tusharkantighorai643@gmail.com>
2025-03-05 19:38:58 +0530

    Done

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -53,4 +53,43 @@ router.get('/:tasteType', async(req,res)=>{
 
     }
 })
+
+router.put('/:id', async (req, res)=>{
+    try{
+        const menuId = req.params.id; // Extract the id of Menu Item from the URL parameter
+        const updatedMenuData = req.body; // Updated data for the Menu Item
+
+        const response = await MenuItem.findByIdAndUpdate(menuId, updatedMenuData, {
+            new: true, // Return the updated document
+            runValidators: true, // Run Mongoose validation
+        })
+
+        if (!response) {
+            return res.status(404).json({ error: 'Menu Item not found' });
+        }
+
+        console.log('data updated');
+        res.status(200).json(response);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const menuId = req.params.id; // Extract the Menu's ID from the URL parameter
+        
+        // Assuming you have a MenuItem model
+        const response = await MenuItem.findByIdAndRemove(menuId);
+        if (!response) {
+            return res.status(404).json({ error: 'Menu Item not found' });
+        }
+        console.log('data delete');
+        res.status(200).json({message: 'Menu Deleted Successfully'});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
 module.exports=router;
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const personRoutes = require('./routes/personRoutes');
 const menuItemRoutes = require('./routes/menuItemRoutes');
 
 // Use the routers
-app.use('/person',localAuthMiddleware, personRoutes);
+app.use('/person',personRoutes);
 app.use('/menu', menuItemRoutes);
   
 app.listen(PORT, ()=>{
